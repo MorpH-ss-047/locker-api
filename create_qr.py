3f3d28a0bb3e2f95f0ec7c06a69f98c22a563638
Sagar <sagarmohanty5509@gmail.com>
2023-05-02 18:04:29 +0530

    sagar commit

---
+++ create_qr.py
@@ -0,0 +1,173 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from urllib.parse import urlencode
+import requests
+import json
+
+import config
+
+
+credentials = service_account.Credentials.from_service_account_file(
+    config.SERVICE_ACCOUNT_FILE, scopes=config.SCOPES
+)
+
+androidmanagement = build("androidmanagement", "v1", credentials=credentials)
+
+
+def create_policy(pk, policy_json):
+    new_policy_name = config.BASE_POLICY_NAME + pk
+    policy = (
+        androidmanagement.enterprises()
+        .policies()
+        .patch(
+            name=new_policy_name,
+            body=json.loads(policy_json),
+        )
+        .execute()
+    )
+
+    return policy, new_policy_name
+
+
+def get_policy(pk):
+    policy_name = config.BASE_POLICY_NAME + pk
+    try:
+        policy = (
+            androidmanagement.enterprises()
+            .policies()
+            .get(
+                name=policy_name,
+            )
+            .execute()
+        )
+        return policy
+    except Exception as e:
+        return False
+
+
+def get_enrollment_token(enterprise_name, policy_name):
+    enrollment_token = (
+        androidmanagement.enterprises()
+        .enrollmentTokens()
+        .create(parent=enterprise_name, body={"policyName": policy_name})
+        .execute()
+    )
+    return enrollment_token
+
+
+def get_qr_code(enrollment_token):
+    image = {"cht": "qr", "chs": "500x500", "chl": enrollment_token["qrCode"]}
+    qrcode_url = "https://chart.googleapis.com/chart?" + urlencode(image)
+    return qrcode_url
+
+
+def get_qr_url(pk):
+    try:
+        _, policy_name = create_policy(pk, config.POLICY_JSON)
+    except Exception as e:
+        return False
+        # return f"Something went wrong during policy creation {e}", 403
+    try:
+        enrollment_token = get_enrollment_token(config.ENTERPRISE_NAME, policy_name)
+    except Exception as e:
+        return False
+        # return f"Something went wrong during enrollment token creation {e}", 403
+    try:
+        qr_code_url = get_qr_code(enrollment_token)
+        return qr_code_url
+    except Exception as e:
+        return False
+        # return f"Something went wrong during qr code creation {e}", 403
+
+
+def update_policy(pk, new_policy_json):
+    policy_name = config.BASE_POLICY_NAME + pk
+
+    policy = get_policy(pk)
+    new_policy = new_policy_json
+    
+    if("lockdownDevice" in new_policy.keys()):
+      
+        for i, app2 in enumerate(policy["applications"]):
+            if app2["packageName"] == "com.apptimates.apptimatelocker":
+                policy["applications"][i]['installType'] = 'KIOSK' if new_policy['lockdownDevice'] else 'FORCE_INSTALLED'
+                updated_policy_dict = policy
+                break
+        updated_policy_dict['screenCaptureDisabled'] = new_policy['lockdownDevice'] 
+        updated_policy_dict["usbFileTransferDisabled"] = new_policy['lockdownDevice'] 
+        updated_policy_dict["bluetoothConfigDisabled"] = new_policy['lockdownDevice'] 
+        updated_policy_dict["outgoingBeamDisabled"] = new_policy['lockdownDevice'] 
+        updated_policy_dict["cameraDisabled"] = new_policy['lockdownDevice'] 
+
+            
+    else:
+
+        updated_policy_dict = {**policy, **new_policy}
+
+    updated_policy = (
+        androidmanagement.enterprises()
+        .policies()
+        .patch(
+            name=policy_name,
+            body=updated_policy_dict,
+        )
+        .execute()
+    )
+
+    return updated_policy
+
+
+def release_device(pk):
+    policy_name = config.BASE_POLICY_NAME + pk
+
+    policy = get_policy(pk)
+   
+    for i, app2 in enumerate(policy["applications"]):
+        if app2["packageName"] == "com.apptimates.apptimatelocker":
+            policy["applications"][i]['installType'] = 'BLOCKED' 
+            updated_policy_dict = policy
+            break
+    updated_policy_dict['factoryResetDisabled'] = False
+    updated_policy_dict['screenCaptureDisabled'] = False
+    updated_policy_dict["usbFileTransferDisabled"] = False
+    updated_policy_dict["bluetoothConfigDisabled"] = False
+    updated_policy_dict["outgoingBeamDisabled"] = False
+    updated_policy_dict["cameraDisabled"] = False
+
+    updated_policy = (
+        androidmanagement.enterprises()
+        .policies()
+        .patch(
+            name=policy_name,
+            body=updated_policy_dict,
+        )
+        .execute()
+    )
+    print (updated_policy)
+
+    #check if the policy was updated successfully
+    keys = updated_policy.keys()
+    return (
+        # (if ("factoryResetDisabled") in keys updated_policy['factoryResetDisabled'] == False else False )and
+        # updated_policy['screenCaptureDisabled'] == False and
+        # updated_policy["usbFileTransferDisabled"] == False and
+        # updated_policy["bluetoothConfigDisabled"] == False and
+        # updated_policy["outgoingBeamDisabled"] == False and
+        # updated_policy["cameraDisabled"] == False 
+        all([policy["applications"][i]['installType'] == 'BLOCKED' for i, app2 in enumerate(policy["applications"]) if app2["packageName"] == "com.apptimates.apptimatelocker"])
+            
+    )
+        
+    # return updated_policy
+
+
+if __name__ == "__main__":
+    # _, policy_name = create_policy("mypolicy", config.POLICY_JSON)
+    # enrollment_token = get_enrollment_token(
+    #     config.ENTERPRISE_NAME, policy_name
+    # )
+    # qr_code_url = get_qr_code(enrollment_token)
+    # r = requests.get(qr_code_url)
+    # with open("qr_code.png", "wb") as f:
+    #     f.write(r.content)
+    print(get_policy("mypolicy"))
---
+++ flask_api.py
@@ -0,0 +1,119 @@
+from flask import Flask, request, jsonify
+import json
+import create_qr as cq
+
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/get_qr", methods=["GET", "POST"])
+def get_qr():
+    print(request.method)
+    try:
+        data = request.args
+        pk = data["pk"]
+        if(not pk):
+            obj={"error":1,"message":"KEY IS EMPTY","status":500}
+            return jsonify(obj), 500
+        qr_code = cq.get_qr_url(pk)
+        if(qr_code==False):
+            obj={"error":1,"message":"Error Generating qr try again","status":403}
+            return jsonify(obj), 403
+        obj={"error":0,"message":"QR generated successfully","status":200,"qr_code":qr_code}
+        return jsonify(obj),200
+    except Exception as e:
+        obj={"error":1,"message":"KEY NOT FOUND","status":500}
+        return jsonify(obj), 500
+    
+
+
+@app.route("/api/v1/get_policy", methods=["GET", "POST"])
+def get_policy():
+    print(request.method)
+    try:
+        data = request.args
+        pk = data["pk"]
+        if(not pk):
+            obj={"error":1,"message":"KEY IS EMPTY","status":500}
+            return jsonify(obj), 500
+        policy = cq.get_policy(pk)
+        if(policy==False):
+            obj={"error":1,"message":"No Policy found for given Policy name","status":403}
+            return jsonify(obj), 403
+        obj={"error":0,"message":"Policy fetched successfully","status":200,"policy":policy}
+        return jsonify(obj),200
+       
+    except Exception as e:
+        obj={"error":1,"message":"KEY NOT FOUND","status":500}
+        return jsonify(obj), 500
+
+
+@app.route("/api/v1/update_policy", methods=[ "POST"])
+def update_policy():
+    print(request.method)
+    try:
+        data = request.args
+        # print(request.json)
+        body=request.get_json()
+        # print(body)
+        if(not body):
+            obj={"error":1,"message":"BODY IS EMPTY","status":500}
+            return jsonify(obj), 500
+        pk = data["pk"]
+        if(not pk):
+            obj={"error":1,"message":"KEY IS EMPTY","status":500}
+            return jsonify(obj), 500
+        new_policy_json =body
+
+        if not all([pk, new_policy_json]):
+            obj={"error":1,"message":"Both pk and new_policy_json are required","status":500}
+            return jsonify(obj), 500
+        
+        try:
+            policy = cq.update_policy(pk=pk, new_policy_json=new_policy_json)
+            obj={"error":0,"message":"Policy updated successfully","status":200}
+            
+            return jsonify(obj),200
+        except Exception as e:
+            obj={"error":1,"message":"Not able to update Policy",e:e,"status":500}
+            return jsonify(obj), 500
+    except Exception as e:
+        obj={"error":1,"message":"Error Occured","status":500}
+        return jsonify(obj), 500
+
+@app.route("/api/v1/release_device", methods=[ "GET", "POST"])
+def release_device():
+    try:
+        data = request.args
+        pk = data["pk"]
+        print(pk)
+        
+        if(not pk):
+            obj={"error":1,"message":"KEY IS EMPTY","status":500}
+            return jsonify(obj), 500
+    
+        
+        try:
+            policy = cq.release_device(pk=pk)
+            if(policy==False):
+                obj={"error":1,"message":"No Policy found for given Policy name","status":403}
+                return jsonify(obj), 403
+            obj={"error":0,"message":"Policy updated successfully","status":200}
+            return jsonify(obj),200
+        except Exception as e:
+            obj={"error":1,"message":"Not able to update Policy",e:e,"status":500}
+            return jsonify(obj), 500
+    except Exception as e:
+        obj={"error":1,"message":"Some error occured","status":500}
+        return jsonify(obj), 500
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def say_hi():
+    obj={"error":0,"message":"API WORKING","status":200}
+    return jsonify(obj), 200
+
+
+if __name__ == "__main__":
+    app.run(threaded=True,debug=True, host="0.0.0.0", port=5001)
